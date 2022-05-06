1b46a04410b47292156782efd0272695512ed58d
Michael Bryant <m.alexander.bryant@gmail.com>
2022-05-06 10:53:13 -0700

    modified py to scrape multiple albums for reviews

--- scraper/retrieve_reviews.py
+++ scraper/retrieve_reviews.py
@@ -1,8 +1,45 @@
-import scraper_reviews as rym
+from selenium import webdriver
+import time
 import pandas as pd
+import numpy as np
+from selenium.common.exceptions import NoSuchElementException 
+import scraper_reviews as rym
 
-df = rym.get_album_reviews("https://rateyourmusic.com/release/album/pink-floyd/wish-you-were-here/reviews/1/", 1015,0)
 
-df.to_csv('../scraped_reviews/wish_you_were_here_reviews.csv', index=False)
+    
+    
+df_album_info = pd.read_csv("rym_top_5000_all_time.csv") 
+
+df_album_info = df_album_info[['Album', 'Artist Name', 'Number of Reviews']]
+
+
+transformed_albums = []
+for album in df_album_info['Album']:
+    temp = album.replace(" ", "-").replace("&","and").replace(",","").replace(".","_").replace("!","").replace("(","").replace(")","").replace("'","").replace("[","").replace("]","").lower()
+    transformed_albums.append(temp)
+    
+transformed_artists = []
+for artist in df_album_info['Artist Name']:
+    temp = artist.replace(" ", "-").replace("&","and").replace(",","").replace(".","_").replace("!","").replace("(","").replace(")","").replace("'","").replace("[","").replace("]","").lower()
+    transformed_artists.append(temp)
+
+urls = []
+for i in range(len(transformed_artists)):
+    url = "https://rateyourmusic.com/release/album/{}/{}/reviews/1/".format(transformed_artists[i],transformed_albums[i])
+    urls.append(url)
+    
+df_urls = pd.concat([pd.Series(urls), df_album_info['Number of Reviews']], axis=1)
+
+reviews = pd.DataFrame()
+
+for idx in range(len(df_urls)):
+    try:
+        df = rym.get_album_reviews(df_urls[0][idx], df_urls['Number of Reviews'][idx])
+        reviews=pd.concat([reviews,df],axis=0)
+    except:
+        pass
+        
+reviews.to_csv('../scraped_reviews/album_reviews.csv', index=False)
+
 
 
--- scraper/scraper_reviews.py
+++ scraper/scraper_reviews.py
@@ -6,7 +6,7 @@ import numpy as np
 from selenium.common.exceptions import NoSuchElementException 
 
 #function for requesting number of reviews
-def get_album_reviews(review_page_url, num_reviews, verbose):
+def get_album_reviews(review_page_url, num_reviews, verbose=0):
     
     '''Gathers albums as a dataframe, scraped from rateyourmusic.com'''   
     
@@ -16,77 +16,88 @@ def get_album_reviews(review_page_url, num_reviews, verbose):
 
     #url to begin scraping from
     url = review_page_url
-    driver.get(url)
+    try:
+        driver.get(url)
+    except:
+        driver.quit()
+        return None
     
     #wait for browser to open
-    time.sleep(10)
+    time.sleep(5)
     
     #initialize reviews array
     reviews = []
     
-    #while number of collected reviews does not equal number of requested reviews 
-    while len(reviews) != num_reviews:
-        
-        #get review ids
-        review_ids = driver.find_elements_by_xpath('.//div[@id="column_container_right"]//div[contains(@id,"reviews")]//div[contains(@id, "std")]')
-        
-        #for each review
-        for ids in review_ids:
+    try:
+        #while number of collected reviews does not equal number of requested reviews 
+        while len(reviews) != num_reviews:
             
-            #determine the location of the review
-            location = ids.get_attribute('id')
+            #get review ids
+            review_ids = driver.find_elements_by_xpath('.//div[@id="column_container_right"]//div[contains(@id,"reviews")]//div[contains(@id, "std")]')
             
-            #xpath to specify review location
-            xpath = './/div[@id="'
-            xpath += str(location)
-            xpath += '"]'
-            
-            #initialize variable to break while loop
-            collected_successfully = False
-        
-            #runs until collected_successfully = True
-            while not collected_successfully:
+            #for each review
+            for ids in review_ids:
                 
-                #try to find items for scraping
-                try:
+                #determine the location of the review
+                location = ids.get_attribute('id')
+                
+                #xpath to specify review location
+                xpath = './/div[@id="'
+                xpath += str(location)
+                xpath += '"]'
+                
+                #initialize variable to break while loop
+                collected_successfully = False
+            
+                #runs until collected_successfully = True
+                while not collected_successfully:
                     
-                    review = []
-                    user = driver.find_element_by_xpath('{}//div[contains(@class, "review_header")]//a[contains(@class, "user")]'.format(xpath)).text
-                    review_loc = driver.find_elements_by_xpath('{}//div[contains(@class, "body")]//span[contains(@class, "rendered")]'.format(xpath))
-                    
-                    #reviews are in list form so append
-                    for value in review_loc:
-                        review.append(value.text)
-                    
-                    #if rating exists record otherwise set to nan
+                    #try to find items for scraping
                     try:
-                        rating_loc = driver.find_element_by_xpath('{}//div[contains(@class, "header")]//span[contains(@class, "rating")]/img[@width="90"]'.format(xpath))
-                        rating = rating_loc.get_attribute("title")
-                    except NoSuchElementException:
-                        rating = np.nan
                         
-                    #set true to break while loop
-                    collected_successfully = True
-                
-                #if page hasn't loaded yet wait 5 seconds
-                except:
-                    time.sleep(5)
-    
-            #append scraped information to reviews array
-            reviews.append({"User": user,
-                            "Review": review,
-                            "Rating": rating})
+                        review = []
+                        user = driver.find_element_by_xpath('{}//div[contains(@class, "review_header")]//a[contains(@class, "user")]'.format(xpath)).text
+                        review_loc = driver.find_elements_by_xpath('{}//div[contains(@class, "body")]//span[contains(@class, "rendered")]'.format(xpath))
+                        
+                        #reviews are in list form so append
+                        for value in review_loc:
+                            review.append(value.text)
+                        
+                        #if rating exists record otherwise set to nan
+                        try:
+                            rating_loc = driver.find_element_by_xpath('{}//div[contains(@class, "header")]//span[contains(@class, "rating")]/img[@width="90"]'.format(xpath))
+                            rating = rating_loc.get_attribute("title")
+                        except NoSuchElementException:
+                            rating = np.nan
+                            
+                        #set true to break while loop
+                        collected_successfully = True
+                    
+                    #if page hasn't loaded yet wait 5 seconds
+                    except:
+                        time.sleep(5)
         
-        #click next page
-        try:
-            driver.find_element_by_xpath('.//a[@class="navlinknext"]').click()
-        
-            time.sleep(5)
-        
-        #if on last page then break from if condition
-        except NoSuchElementException:
+                #append scraped information to reviews array
+                reviews.append({"User": user,
+                                "Review": review,
+                                "Rating": rating})
             
-            break
+            #click next page
+            try:
+                driver.find_element_by_xpath('.//a[@class="navlinknext"]').click()
+            
+                time.sleep(1)
+            
+            #if on last page then break from if condition
+            except NoSuchElementException:
+                
+                break
+    except:
+        driver.quit()
+        return pd.DataFrame(reviews)
+        
+    # Close browser after scraping has finished.    
+    driver.quit()
         
     #return requested reviews as a dataframe
     return pd.DataFrame(reviews)
